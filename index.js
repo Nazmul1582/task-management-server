9d0a4de73c804ab79248f52e501391a72f9f6387
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-24 22:48:46 +0600

    created a get api for get a specific task by id

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get("/tasks/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await taskCollection.findOne(query);
+      res.send(result)
+    })
+
     app.post("/tasks", async(req, res) => {
         const newTask = req.body;
         const result = await taskCollection.insertOne(newTask);
